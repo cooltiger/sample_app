27cc8ec671e603470e4273b15d0dcd2065bc577d
cooltiger <cooltigerjp@gmail.com>
2014-12-24 22:13:20 +0900

    finish 10.18 validate micropost

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,6 +2,7 @@ class Micropost < ActiveRecord::Base
   belongs_to :user
   attr_accessible :content
   validates :user_id, presence: true
+  validates :content, presence: true, length: { maximum: 140 }
 
-  default_scope -> { order 'microposts.created_at DESC' }
+  default_scope  { order 'microposts.created_at DESC' }
 end
